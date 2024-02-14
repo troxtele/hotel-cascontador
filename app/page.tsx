57bff989bde5f04ad08339ca0e1837d27ee938cb
troxtele <indomisba@gmail.com>
2024-02-14 10:12:19 +0600

    ggd

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useRef } from "react";
-import "./App.css";
+import "./app.css";
 import Navbar from "./components/Navbar";
 // import Hero from "./components/Hero";
 // import Gallery from "./components/Gallery";
